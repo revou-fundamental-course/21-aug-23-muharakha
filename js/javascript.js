9a4845cc5132b7a8614a0e92a5e0d9eb5ca39430
muharakha <muhammadrakha004@gmail.com>
2023-08-30 22:58:53 +0700

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,51 @@
+        
+        //JS slideshow gambar <
+        var slideIndex = 1;
+            showDivs(slideIndex);
+
+        function plusDivs(n) {
+            showDivs((slideIndex += n));
+        }
+
+        function showDivs(n) {
+            var i;
+            var imgList = document.getElementsByClassName("img-slideshow");
+            if (n > imgList.length) slideIndex = 1;
+            else if (n < 1) slideIndex = imgList.length;
+
+            for (i = 0; i < imgList.length; i++) {
+                imgList[i].style.display = "none";
+            }
+
+            imgList[slideIndex - 1].style.display = "block";
+        }
+
+        setInterval(() => {
+          plusDivs(1);
+        }, 1000);
+        //JS slideshow gambar>
+
+        //JS get Nama<
+        var name = prompt("Masukan nama kamu");
+            document.getElementById("nama").innerText = name;
+        //JS get Nama<
+
+        //JS FORM get<
+        function validateForm() {
+            var name = document.forms["message-form"]["full-name"].value;
+            var date = document.forms["message-form"]["birth-date"].value;
+            var gender = document.forms["message-form"]["gender"].value;
+            var messages = document.forms["message-form"]["messages"].value;
+
+            if (name == "" || date == "" || gender == "" || messages == "") {
+              alert("Input tidak boleh kosong");
+              return false;
+            }
+
+            document.getElementById("sender-full-name").innerText = name;
+            document.getElementById("sender-birth-date").innerText = date;
+            document.getElementById("sender-gender").innerText = gender;
+            document.getElementById("sender-messages").innerText = messages;
+            return false;
+          }
+          //JS FORM get>
